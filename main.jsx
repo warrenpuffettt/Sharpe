5c193731970f2997fb2e708e492ced6758d1270e
warrenpuffettt <warrenpuffettt@gmail.com>
2026-05-22 00:44:53 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Sharpe from './Sharpe'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Sharpe />
+  </React.StrictMode>
+)
